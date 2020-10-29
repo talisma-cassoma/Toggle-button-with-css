b824e37b9d780ee61f4f852edce953d71fc35b1f
talisma <talisma63@gmail.com>
2020-10-29 01:28:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let body = document.querySelector('body');
+var checkbox = document.querySelector('#dark-mode')
+		checkbox.addEventListener("click", terms_change)
+
+function terms_change(){
+    //If it is checked.
+    if(body.style.backgroundColor == 'whitesmoke'){
+        alert('you atived the page darkmode');
+				checkbox.checked = true;
+							body.style.backgroundColor = '#121519';
+    }
+    //If it has been unchecked.
+    else{
+        alert('you atived the page lightmode');
+				checkbox.checked = false;
+				body.style.backgroundColor = 'whitesmoke';
+							
+    }
+}
+   
